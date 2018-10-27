866917342d683e4faf5a63b60fbfe69d2cd1abf2
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2018-10-27 15:43:02 -0700

    Finished Flashcards activity!

--- src/app/flashcards/flashcards.component.ts
+++ src/app/flashcards/flashcards.component.ts
@@ -8,7 +8,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FlashcardsComponent {
     sound: string; index: number; lenOfWords: number; soundsList: string[];
-    p: string[]; b: string[];
+    p: string[]; b: string[]; m: string[]; d: string[];
+    n: string[]; t: string[]; k: string[]; g: string[];
+    f: string[]; v: string[]; ch: string[]; j: string[];
+    z: string[]; sh: string[]; th: string[]; w: string[];
+    ng: string[]; y: string[]; h: string[]; r: string[];
+    l: string[]; s: string[];
     Flashcards = []; flashcardKey: string; flashcardImg: string;
 
     // Inject the activated route
@@ -23,6 +28,26 @@ export class FlashcardsComponent {
         // Sounds and their words corresponding to their images
         this.p = ['Potato', 'Stamp', 'Ship', 'Apple', 'Dimple', 'Pants', 'Staples', 'Park', 'Pickle', 'Paint'];
         this.b = ['Bat', 'Beetle', 'Cab', 'Bus'];
+        this.m = ['Monkey', 'Moose', 'Dorm'];
+        this.d = ['Dog', 'Dance', 'Chard'];
+        this.n = ['Nickel', 'Nail', 'Den'];
+        this.t = ['Tank', 'Tea', 'Cart'];
+        this.k = ['Kite', 'Rock'];
+        this.g = ['Garden', 'Glass'];
+        this.f = ['Fan', 'Fly'];
+        this.v = ['Violin', 'Vase'];
+        this.ch = ['Chicken', 'Chick'];
+        this.j = ['Juice', 'Jar'];
+        this.z = ['Zoo', 'Zebra'];
+        this.sh = ['Shark', 'Shoes'];
+        this.th = ['Thunder', 'Bathtub'];
+        this.w = ['Water', 'Wave'];
+        this.ng = ['King', 'Sing'];
+        this.y = ['Yolk', 'Boy'];
+        this.h = ['Hair', 'Hawk'];
+        this.r = ['Rabbit', 'Raven'];
+        this.l = ['Lion', 'Lizard'];
+        this.s = ['Saw', 'Sofa'];
 
         switch (this.sound) {
             case 'p':
@@ -32,6 +57,86 @@ export class FlashcardsComponent {
             case 'b':
                 this.soundsList = this.b;
                 this.lenOfWords = this.b.length;
+                break;
+            case 'm':
+                this.soundsList = this.m;
+                this.lenOfWords = this.m.length;
+                break;
+            case 'd':
+                this.soundsList = this.d;
+                this.lenOfWords = this.d.length;
+                break;
+            case 'n':
+                this.soundsList = this.n;
+                this.lenOfWords = this.n.length;
+                break;
+            case 't':
+                this.soundsList = this.t;
+                this.lenOfWords = this.t.length;
+                break;
+            case 'k':
+                this.soundsList = this.k;
+                this.lenOfWords = this.k.length;
+                break;
+            case 'g':
+                this.soundsList = this.g;
+                this.lenOfWords = this.g.length;
+                break;
+            case 'f':
+                this.soundsList = this.f;
+                this.lenOfWords = this.f.length;
+                break;
+            case 'v':
+                this.soundsList = this.v;
+                this.lenOfWords = this.v.length;
+                break;
+            case 'ch':
+                this.soundsList = this.ch;
+                this.lenOfWords = this.ch.length;
+                break;
+            case 'j':
+                this.soundsList = this.j;
+                this.lenOfWords = this.j.length;
+                break;
+            case 'z':
+                this.soundsList = this.z;
+                this.lenOfWords = this.z.length;
+                break;
+            case 'sh':
+                this.soundsList = this.sh;
+                this.lenOfWords = this.sh.length;
+                break;
+            case 'th':
+                this.soundsList = this.th;
+                this.lenOfWords = this.th.length;
+                break;
+            case 'w':
+                this.soundsList = this.w;
+                this.lenOfWords = this.w.length;
+                break;
+            case 'ng':
+                this.soundsList = this.ng;
+                this.lenOfWords = this.ng.length;
+                break;
+            case 'y':
+                this.soundsList = this.y;
+                this.lenOfWords = this.y.length;
+                break;
+            case 'h':
+                this.soundsList = this.h;
+                this.lenOfWords = this.h.length;
+                break;
+            case 'r':
+                this.soundsList = this.r;
+                this.lenOfWords = this.r.length;
+                break;
+            case 'l':
+                this.soundsList = this.l;
+                this.lenOfWords = this.l.length;
+                break;
+            case 's':
+                this.soundsList = this.s;
+                this.lenOfWords = this.s.length;
                 break;
         }
 
